aef82271d1bbf7295e1f3830327e52842a29b395
Nick Hansen <nick@luxor.tech>
2018-09-23 23:26:58 +0200

    fixing loki_round

--- src/cnh_cryptonote_basic/difficulty.cpp
+++ src/cnh_cryptonote_basic/difficulty.cpp
@@ -191,7 +191,7 @@ namespace cryptonote {
     harmonic_mean_D = N / sum_inverse_D;
 
     // Keep LWMA sane in case something unforeseen occurs.
-    if (static_cast<int64_t>(loki_round(LWMA)) < T / 20)
+    if (static_cast<int64_t>(tools::round::loki_round(LWMA)) < T / 20)
       LWMA = static_cast<double>(T / 20);
 
     nextDifficulty = harmonic_mean_D * T / LWMA * adjust;
